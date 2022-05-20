b9f4f5333448deb267cc334765e882aa184ad992
Kelvin Keegan <kelvinkeegen17@gmail.com>
2022-05-20 16:48:49 +0300

    Added Jasypt for added security

---
+++ src/main/java/com/appsbykeegan/watcher_storageapi/config/JasyptConfig.java
@@ -0,0 +1,37 @@
+package com.appsbykeegan.watcher_storageapi.config;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by Kelvin Keegan on 5/20/2022
+ */
+
+@Configuration
+public class JasyptConfig {
+
+    @Value("${my.app.salt}")
+    private String salt;
+
+    @Bean(name = "jasyptStringEncryptor")
+    public StringEncryptor EncryptSensitiveData() {
+
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword(salt);
+        config.setAlgorithm("PBEWithMD5AndDES");
+        config.setKeyObtentionIterations("1000");
+        config.setPoolSize("1");
+        config.setProviderName("SunJCE");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setStringOutputType("base64");
+        encryptor.setConfig(config);
+        return encryptor;
+
+    }
+
+}
--- src/test/java/com/appsbykeegan/watcher_storageapi/WatcherStorageApiApplicationTests.java
+++ src/test/java/com/appsbykeegan/watcher_storageapi/WatcherStorageApiApplicationTests.java
@@ -1,13 +1,37 @@
 package com.appsbykeegan.watcher_storageapi;
 
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class WatcherStorageApiApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void TestEncryptor() {
+
+		PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+		SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+		config.setPassword("banana");
+		config.setAlgorithm("PBEWithMD5AndDES");
+		config.setKeyObtentionIterations("1000");
+		config.setPoolSize("1");
+		config.setProviderName("SunJCE");
+		config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+		config.setStringOutputType("base64");
+		encryptor.setConfig(config);
+
+		String value = "boy";
+		String valueEnc;
+
+		System.out.println(valueEnc = encryptor.encrypt(value));
+		System.out.println(encryptor.decrypt(valueEnc));
+
 	}
 
 }
